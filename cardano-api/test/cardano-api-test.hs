540fd2cdba5bef36595b9228cfda94a9044a9606
Luke Nadur <19835357+intricate@users.noreply.github.com>
2020-09-23 14:52:28 -0400

    Add JSON roundtrip tests

--- cardano-api/test/cardano-api-test.hs
+++ cardano-api/test/cardano-api-test.hs
@@ -8,6 +8,7 @@ import qualified Test.Cardano.Api.MetaData
 import qualified Test.Cardano.Api.Typed.Bech32
 import qualified Test.Cardano.Api.Typed.CBOR
 import qualified Test.Cardano.Api.Typed.Envelope
+import qualified Test.Cardano.Api.Typed.Json
 import qualified Test.Cardano.Api.Typed.MultiSigScript
 import qualified Test.Cardano.Api.Typed.RawBytes
 
@@ -19,6 +20,7 @@ main =
     , Test.Cardano.Api.Typed.Bech32.tests
     , Test.Cardano.Api.Typed.CBOR.tests
     , Test.Cardano.Api.Typed.Envelope.tests
+    , Test.Cardano.Api.Typed.Json.tests
     , Test.Cardano.Api.Typed.MultiSigScript.tests
     , Test.Cardano.Api.Typed.RawBytes.tests
     ]
